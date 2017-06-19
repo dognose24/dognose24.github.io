b5de49a77b39f0bcff15b797321d75056d4bd0d4
dognose <minzong@e-lead.com.tw>
2017-06-19 18:08:43 +0800

    Apply compass project for styles

--- portfolio/src/App.tsx
+++ portfolio/src/App.tsx
@@ -1,16 +1,47 @@
 import * as React from 'react';
-import './App.css';
+import './assets/styles/stylesheets/app.css';
 import Hello, {getExclamationMarks} from './Components/Hello';
 
 const logo = require('./assets/images/logo.svg');
 
+export interface SumProps {
+  name: string;
+  enthusiasmLevel?: number;
+}
+
 class App extends React.Component<{}, null> {
+
+  /**
+   * @param {number[]} nums
+   * @param {number} target
+   * @return {number[]}
+   */
+  twoSum(nums: number[], target: number): number[] | boolean {
+
+    for (let i = 0; i < nums.length; i++) {
+
+      let remainingNum = target - nums[i];
+      let foundIdxForCal = nums.indexOf(remainingNum);
+
+      if (foundIdxForCal > -1 && foundIdxForCal !== i) {
+        return [i, foundIdxForCal];
+      }
+    }
+
+    return [];
+  }
+
   render() {
+
+    let nums = [3, 2, 4], target = 6;
+
+    this.twoSum(nums, target);
+
     return (
       <div className="App">
         <div className="App-header">
           <img src={logo} className="App-logo" alt="logo" />
-          <h2>Welcome to React RR</h2>
+          <h2>Dognose</h2>
         </div>
         <div className="App-intro">
           <code>{getExclamationMarks(5)}</code>
